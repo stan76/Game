cedc8b6ee97cf6b082cadb9bc5ef7421404795e7
stan76 <stanip@live.fr>
2014-01-19 16:04:18 +0100

    Initial commit

---
+++ Dev/game/src/com/ip/game/Game.java
@@ -0,0 +1,31 @@
+package com.ip.game;
+
+
+public class Game extends com.badlogic.gdx.Game {
+
+	
+	public void create() {		
+		
+	}
+
+	public void render() {		
+		super.render();
+	}
+
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+	
+	public void dispose() {
+		super.dispose();
+		
+	}
+
+	public void pause() {
+		super.pause();
+	}
+
+	public void resume() {
+		super.resume();
+	}
+}
